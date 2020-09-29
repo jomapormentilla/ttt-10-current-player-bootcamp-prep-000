df92dd66312b1121c138aca56f8999f0d6514985
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 17:13:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,9 +13,9 @@ end
 
 def current_player(board)
   count = turn_count(board)
-  if( count % 2 == 0 )
-    return "O"
-  else
+  if( count.even? )
     return "X"
+  else
+    return "O"
   end
 end
