9f8c2a0bb8dd4487c9eceec9598b1e7168628adb
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 17:10:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if (space == " " || space == "" || space == nil)
+      nil
+    else
+      count += 1
+    end
+  end
+  
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if( count % 2 === 0 )
+    return "O"
+  else
+    return "X"
+  end
+end
